44c298d10edc0dc8e0124078f80a046714d3ad0f
Roger Davis <roger.davis86@yahoo.com>
2023-09-21 23:44:10 +0000

    updated requirements.txt for deployment

--- Drink_Tank/settings.py
+++ Drink_Tank/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'Drink_Tank.wsgi.application'
 # }
 
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+   'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
