ae80ad706e0a8b82694ab15d92a2325d26bad425
Roger Davis <roger.davis86@yahoo.com>
2023-09-21 23:33:10 +0000

    second deployment commit

--- Drink_Tank/settings.py
+++ Drink_Tank/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["drink-tank.herokuapp.com"]
+ALLOWED_HOSTS = ["drink-tank.herokuapp.com", '8000-rogerwdavis-drinktank-2ndudugpxhw.ws-eu104.gitpod.io']
 
 
 # Application definition
