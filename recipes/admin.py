6e1c7791ea94ac4ffcf63a69d70b22b74d8935fc
Roger Davis <roger.davis86@yahoo.com>
2023-09-22 10:09:39 +0000

    test csfr error

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . import models
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(models.Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    summer_fields = ('content')
 
-admin.site.register(models.Recipe)
+
+
+
