e53c3ba0c377705325fcc1491d0eda1df3cf10e9
Roger Davis <roger.davis86@yahoo.com>
2023-09-22 13:52:53 +0000

    update django

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,5 +5,5 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    prepopulated_fields = {'slug': ('title,')}
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
