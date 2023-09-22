b30e4e345968bb4d03c71455a36aef62cee3f093
Roger Davis <roger.davis86@yahoo.com>
2023-09-22 13:59:03 +0000

    update env.py

--- Drink_Tank/settings.py
+++ Drink_Tank/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEV'
 
 ALLOWED_HOSTS = [ '8000-rogerwdavis-drinktank-2ndudugpxhw.ws-eu104.gitpod.io', 'drink-tank-f365b9a8022a.herokuapp.com']
 
