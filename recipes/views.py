995764fd1c37596907c3f54df30aecd6c1b65c2e
Roger Davis <roger.davis86@yahoo.com>
2023-12-29 17:17:09 +0000

    update recipes/views

--- recipes/views.py
+++ recipes/views.py
@@ -8,26 +8,23 @@ from django.contrib.auth.mixins import (
     UserPassesTestMixin, LoginRequiredMixin
 )
 
-from django.contrib.auth.mixins import LoginRequiredMixin
-
 from .models import Recipe
 from .forms import RecipeForm
-
 
 class Recipes(ListView):
 
     template_name = "index.html"
     model = Recipe
     context_object_name = "recipes"
-    recipes = self.model.objects.all()
-    
+
+    def get_queryset(self):
+        return self.model.objects.all()
 
 class RecipeDetail(DetailView):
 
     template_name = "recipe_detail.html"
     model = Recipe
     context_object_name = "recipe"
-
 
 class AddRecipe(LoginRequiredMixin, CreateView):
 
@@ -40,7 +37,6 @@ class AddRecipe(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
 
-
 class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
  
     template_name = 'redit_recipe.html'
@@ -50,7 +46,6 @@ class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     
     def test_func(self):
         return self.request.user == self.get_object().user
-
 
 class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
  
